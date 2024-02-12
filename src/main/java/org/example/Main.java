bf598b20c16a0035ad49a3b84b4206e1b4788bc8
Oksana Poliakova <it.oksana.poliakova@gmail.com>
2024-02-12 15:26:16 +0200

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+/**
+ * @projectName Default (Template) Project
+ * @author oksanapoliakova on 12.02.2024
+ */public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
